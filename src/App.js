92f406e0caf493405cbb69e29a65cb4305aa475f
Sergey Cherebedov <cherebedov.sergey@gmail.com>
2023-01-16 01:23:39 +0300

    All done, tests passed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,124 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 import './styles.css';
 
+import {getGrid, getSelected, getMergedGrid, getSeparatedGrid, getCellKey, getCellFromTarget} from './utils';
 
 export const App = () => {
-  const grid = [['First cell']];
+    const searchParams = new URLSearchParams(document.location.search);
+    const width = searchParams.get('width');
+    const height = searchParams.get('height');
 
-  return (
-    <div>
-      <div className='controls'>
-        <button data-merge-button>Merge</button>
-        <button data-separate-button>Separate</button>
-      </div>
-      <table>
-        <tbody>
-          {grid.map((gridRow, rowIndex) => (
-            <tr key={rowIndex}>
-              {gridRow.map((gridCell, colIndex) => (
-                <td
-                  data-selected={false}
-                  data-row-index={rowIndex}
-                  data-col-index={colIndex}
-                  key={colIndex}
-                  colSpan={1}
-                  rowSpan={1}
+    const [grid, setGrid] = useState([]);
+    const [selection, setSelection] = useState([]);
+    const [isMoving, setMoving] = useState(false);
+    const [selectedSet, setSelectedSet] = useState(new Set());
+
+    useEffect(() => {
+        setGrid(getGrid(width, height));
+    }, [width, height]);
+
+    useEffect(() => {
+        if (selection.length === 2) {
+            setSelectedSet(getSelected(grid, selection));
+        }
+    }, [selection, grid]);
+
+    const isSelected = (cellKey) => {
+        return selectedSet.has(cellKey);
+    }
+
+    const onMergeClick = () => {
+        if (selection.length > 1) {
+            const result = getMergedGrid(grid, selectedSet);
+            setGrid(result.grid);
+            setSelection(result.selection);
+        }
+    }
+
+    const onSeparateClick = () => {
+        if (selection.length > 1) {
+            const result = getSeparatedGrid(grid, selectedSet);
+            setGrid(result.grid);
+            setSelection(result.selection);
+        }
+    }
+
+    const onDown = (e) => {
+        const {rowIndex, colIndex} = getCellFromTarget(e.target);
+        setSelection([[rowIndex, colIndex]]);
+        setMoving(true);
+    }
+
+    const onUp = (e) => {
+        setMoving(false);
+        const {rowIndex, colIndex} = getCellFromTarget(e.target);
+
+        if (selection.length < 2 || rowIndex !== selection[1][0] || colIndex !== selection[1][1]) {
+            setSelection((current) => [current[0], [rowIndex, colIndex]]);
+        }
+    }
+
+    const onMove = (e) => {
+        if (isMoving && selection.length) {
+            const {rowIndex, colIndex} = getCellFromTarget(e.target);
+
+            if (selection.length < 2 || rowIndex !== selection[1][0] || colIndex !== selection[1][1]) {
+                setSelection((current) => [current[0], [rowIndex, colIndex]]);
+            }
+        }
+    }
+
+    return (
+        <div>
+            <div className='controls'>
+                <button
+                    data-merge-button
+                    onClick={onMergeClick}
+                    disabled={selection.length < 2}
                 >
-                  {gridCell}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+                    Merge
+                </button>
+                <button
+                    data-separate-button
+                    onClick={onSeparateClick}
+                    disabled={selection.length < 2}
+                >
+                    Separate
+                </button>
+            </div>
+            <table
+                onMouseDown={onDown}
+                onMouseUp={onUp}
+                onMouseMove={onMove}
+            >
+                <tbody>
+                {grid.map((gridRow, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {gridRow.map((gridCell, colIndex) => {
+                            if (gridCell.parent) {
+                                return null;
+                            }
+
+                            const cellKey = getCellKey(rowIndex, colIndex);
+
+                            return (
+                                <td
+                                    data-selected={isSelected(cellKey)}
+                                    data-row-index={rowIndex}
+                                    data-col-index={colIndex}
+                                    key={cellKey}
+                                    colSpan={gridCell.colSpan}
+                                    rowSpan={gridCell.rowSpan}
+                                >
+                                    row: {rowIndex} col: {colIndex}
+                                </td>
+                            );
+                        })}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
---
+++ src/utils.js
@@ -0,0 +1,187 @@
+export function getGrid(width, height) {
+    const grid = [];
+
+    for (let row = 0; row < height; row++) {
+        grid.push([]);
+        for (let col = 0; col < width; col++) {
+            grid[row].push({
+                parent: null,
+                colSpan: 1,
+                rowSpan: 1,
+            });
+        }
+    }
+
+    return grid;
+}
+
+export function getSelected(grid, selection, selected = new Set(), processedRoots = new Set()) {
+    const [startCell, endCell] = selection;
+
+    const rowStart = startCell[0] < endCell[0] ? startCell[0] : endCell[0];
+    const rowEnd = startCell[0] < endCell[0] ? endCell[0] : startCell[0];
+
+    const colStart = startCell[1] < endCell[1] ? startCell[1] : endCell[1];
+    const colEnd = startCell[1] < endCell[1] ? endCell[1] : startCell[1];
+
+    for (let row = rowStart; row <= rowEnd; row++) {
+        for (let col = colStart; col <= colEnd; col++) {
+            const cell = grid[row][col];
+
+            const cellKey = getCellKey(row, col);
+
+            if (!selected.has(cellKey)) {
+                selected.add(cellKey);
+            }
+
+            if (cell.root && !processedRoots.has(cellKey)) {
+                processedRoots.add(cellKey);
+                getSelected(
+                    grid,
+                    [
+                        [rowStart, colStart],
+                        [Math.max(...[row + cell.rowSpan - 1, rowEnd]), Math.max(...[col + cell.colSpan - 1, colEnd])],
+                    ],
+                    selected,
+                    processedRoots,
+                );
+            }
+            if (cell.parent) {
+                const parentRow = cell.parent[0];
+                const parentCol = cell.parent[1];
+                const parentKey = getCellKey(parentRow, parentCol);
+                const parentCell = grid[parentRow][parentCol];
+
+                if (!processedRoots.has(parentKey)) {
+                    processedRoots.add(parentKey);
+                    getSelected(
+                        grid,
+                        [
+                            [Math.min(...[rowStart, parentRow]), Math.min(...[colStart, parentCol])],
+                            [Math.max(...[parentRow + parentCell.rowSpan - 1, rowEnd]), Math.max(...[parentCol + parentCell.colSpan - 1, colEnd])],
+                        ],
+                        selected,
+                        processedRoots,
+                    );
+                }
+            }
+        }
+    }
+
+    return selected;
+}
+
+export function getCellKey(rowIndex, colIndex) {
+    return `${rowIndex}-${colIndex}`;
+}
+
+export function getMergedGrid(grid, selectedSet) {
+    let minRow = grid.length - 1;
+    let minCol = grid[0].length - 1;
+
+    let maxRow = 0;
+    let maxCol = 0;
+
+    grid.forEach((gridRow, rowIndex) => gridRow.forEach((gridCell, colIndex) => {
+        if (selectedSet.has(getCellKey(rowIndex, colIndex))) {
+            if (rowIndex < minRow) {
+                minRow = rowIndex;
+            }
+            if (colIndex < minCol) {
+                minCol = colIndex;
+            }
+            if (rowIndex > maxRow) {
+                maxRow = rowIndex;
+            }
+            if (colIndex > maxCol) {
+                maxCol = colIndex;
+            }
+        }
+    }));
+
+    const mergedGrid = grid.map((gridRow, rowIndex) => gridRow.map((gridCell, colIndex) => {
+        if (selectedSet.has(getCellKey(rowIndex, colIndex))) {
+            const current = rowIndex === minRow && colIndex === minCol;
+
+            return {
+                root: current,
+                parent: current ? null : [minRow, minCol],
+                colSpan: current ? (maxCol - minCol + 1) : 1,
+                rowSpan: current ? (maxRow - minRow + 1) : 1,
+            }
+        }
+
+        return gridCell;
+    }));
+
+    return {
+        grid: mergedGrid,
+        selection: [
+            [
+                minRow,
+                minCol,
+            ],
+            [
+                maxRow,
+                maxCol,
+            ],
+        ],
+    };
+}
+
+export function getSeparatedGrid(grid, selectedSet) {
+    let minRow = grid.length - 1;
+    let minCol = grid[0].length - 1;
+
+    let maxRow = 0;
+    let maxCol = 0;
+
+    const separatedGrid = grid.map((gridRow, rowIndex) => gridRow.map((gridCell, colIndex) => {
+        if (selectedSet.has(getCellKey(rowIndex, colIndex))) {
+            if (rowIndex < minRow) {
+                minRow = rowIndex;
+            }
+            if (colIndex < minCol) {
+                minCol = colIndex;
+            }
+            if (rowIndex > maxRow) {
+                maxRow = rowIndex;
+            }
+            if (colIndex > maxCol) {
+                maxCol = colIndex;
+            }
+
+            return {
+                parent: null,
+                colSpan: 1,
+                rowSpan: 1,
+            }
+        }
+
+        return gridCell;
+    }));
+
+    return {
+        grid: separatedGrid,
+        selection: [
+            [
+                minRow,
+                minCol,
+            ],
+            [
+                maxRow,
+                maxCol,
+            ],
+        ],
+    };
+}
+
+export function getCellFromTarget(target) {
+    const rowIndex = parseInt(target.getAttribute('data-row-index'));
+    const colIndex = parseInt(target.getAttribute('data-col-index'));
+
+    return {
+        rowIndex,
+        colIndex,
+    };
+}
